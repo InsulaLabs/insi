2e485a293e910a57c0aa3d932a5071c4709febdb
bosley <bosley@insulalabs.com>
2025-06-21 17:07:31 -0400

    removed js

--- scripts/utils/get-usage.js
+++
@@ -1,6 +0,0 @@
-var usage = admin.getLimits().usage;
-var max_limits = admin.getLimits().max_limits;
-test.Yay("Usage: " + JSON.stringify(usage));
-test.Yay("Max Limits: " + JSON.stringify(max_limits));
-
-0;
