5e2ed27573c1c2414f0c8306558260df1e852a0a
bosley <bosley@insulalabs.com>
2025-07-31 11:20:29 -0400

    started ferry

---
+++ ferry/errors.go
@@ -0,0 +1,85 @@
+package ferry
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/InsulaLabs/insi/client"
+)
+
+var (
+	ErrEmptyScopePush = errors.New("empty scope push")
+	ErrEndScope       = errors.New("end scope")
+
+	// Ferry-specific errors that correspond to client errors
+	ErrKeyNotFound         = errors.New("key not found")
+	ErrConflict            = errors.New("operation failed due to a conflict")
+	ErrRateLimited         = errors.New("rate limited")
+	ErrDiskLimitExceeded   = errors.New("disk limit exceeded")
+	ErrMemoryLimitExceeded = errors.New("memory limit exceeded")
+	ErrEventsLimitExceeded = errors.New("events limit exceeded")
+	ErrAPIKeyNotFound      = errors.New("api key not found")
+)
+
+func translateError(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if errors.Is(err, client.ErrKeyNotFound) {
+		return ErrKeyNotFound
+	}
+	if errors.Is(err, client.ErrConflict) {
+		return ErrConflict
+	}
+	if errors.Is(err, client.ErrAPIKeyNotFound) {
+		return ErrAPIKeyNotFound
+	}
+
+	var rateLimitErr *client.ErrRateLimited
+	if errors.As(err, &rateLimitErr) {
+		return fmt.Errorf(
+			"%w: %s. Try again in %v. (Limit: %d, Burst: %d)",
+			ErrRateLimited,
+			rateLimitErr.Message,
+			rateLimitErr.RetryAfter,
+			rateLimitErr.Limit,
+			rateLimitErr.Burst,
+		)
+	}
+
+	var diskLimitErr *client.ErrDiskLimitExceeded
+	if errors.As(err, &diskLimitErr) {
+		return fmt.Errorf(
+			"%w: %s (current: %d, limit: %d)",
+			ErrDiskLimitExceeded,
+			diskLimitErr.Message,
+			diskLimitErr.CurrentUsage,
+			diskLimitErr.Limit,
+		)
+	}
+
+	var memoryLimitErr *client.ErrMemoryLimitExceeded
+	if errors.As(err, &memoryLimitErr) {
+		return fmt.Errorf(
+			"%w: %s (current: %d, limit: %d)",
+			ErrMemoryLimitExceeded,
+			memoryLimitErr.Message,
+			memoryLimitErr.CurrentUsage,
+			memoryLimitErr.Limit,
+		)
+	}
+
+	var eventsLimitErr *client.ErrEventsLimitExceeded
+	if errors.As(err, &eventsLimitErr) {
+		return fmt.Errorf(
+			"%w: %s (current: %d, limit: %d)",
+			ErrEventsLimitExceeded,
+			eventsLimitErr.Message,
+			eventsLimitErr.CurrentUsage,
+			eventsLimitErr.Limit,
+		)
+	}
+
+	return err
+}
---
+++ ferry/values.go
@@ -0,0 +1,180 @@
+package ferry
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+	"strings"
+	"sync/atomic"
+
+	"github.com/InsulaLabs/insi/client"
+)
+
+type ValueController[T any] interface {
+	PushScope(prefix string) error
+	PopScope() error
+	GetPrefix() string
+
+	Get(ctx context.Context, key string) (T, error)
+	Set(ctx context.Context, key string, value T) error
+	Delete(ctx context.Context, key string) error
+	CompareAndSwap(ctx context.Context, key string, oldValue, newValue T) error
+	Bump(ctx context.Context, key string, value int) error
+	IterateByPrefix(ctx context.Context, prefix string, offset, limit int) ([]string, error)
+}
+
+type vcImpl[T any] struct {
+	defaultT T
+	client   *client.Client
+	logger   *slog.Logger
+
+	scopes       []string
+	prefix       string
+	scopeChanged atomic.Bool
+}
+
+func NewValueController[T any](defaultT T, c *client.Client, logger *slog.Logger) ValueController[T] {
+	return &vcImpl[T]{
+		defaultT: defaultT,
+		client:   c,
+		logger:   logger.WithGroup("value_controller"),
+	}
+}
+
+func (vc *vcImpl[T]) buildPrefix(key string) string {
+	if len(vc.scopes) == 0 {
+		return key
+	}
+	if !vc.scopeChanged.Load() {
+		return fmt.Sprintf("%s:%s", vc.prefix, key)
+	}
+
+	vc.prefix = strings.Join(vc.scopes, ":")
+	vc.scopeChanged.Store(false)
+	return fmt.Sprintf("%s:%s", vc.prefix, key)
+}
+
+func (vc *vcImpl[T]) GetPrefix() string {
+	return vc.prefix
+}
+
+func (vc *vcImpl[T]) PushScope(prefix string) error {
+	if prefix == "" {
+		return ErrEmptyScopePush
+	}
+
+	vc.scopes = append(vc.scopes, prefix)
+	vc.scopeChanged.Store(true)
+	return nil
+}
+
+func (vc *vcImpl[T]) PopScope() error {
+	if len(vc.scopes) == 0 {
+		return ErrEndScope
+	}
+
+	vc.scopes = vc.scopes[:len(vc.scopes)-1]
+	vc.scopeChanged.Store(true)
+	return nil
+}
+
+func (vc *vcImpl[T]) Get(ctx context.Context, key string) (T, error) {
+	var result T
+
+	fullKey := vc.buildPrefix(key)
+
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		valueStr, err := vc.client.Get(fullKey)
+		if err != nil {
+			if errors.Is(err, client.ErrKeyNotFound) {
+				result = vc.defaultT
+				return nil
+			}
+			return err
+		}
+
+		// Try to unmarshal the JSON value
+		if err := json.Unmarshal([]byte(valueStr), &result); err != nil {
+			vc.logger.Error("Failed to unmarshal value", "key", key, "error", err)
+			result = vc.defaultT
+			return nil
+		}
+
+		return nil
+	})
+
+	return result, translateError(err)
+}
+
+func (vc *vcImpl[T]) Set(ctx context.Context, key string, value T) error {
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		// Marshal the value to JSON
+		valueBytes, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+
+		fullKey := vc.buildPrefix(key)
+
+		return vc.client.Set(fullKey, string(valueBytes))
+	})
+
+	return translateError(err)
+}
+
+func (vc *vcImpl[T]) Delete(ctx context.Context, key string) error {
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		fullKey := vc.buildPrefix(key)
+		return vc.client.Delete(fullKey)
+	})
+
+	return translateError(err)
+}
+
+func (vc *vcImpl[T]) CompareAndSwap(ctx context.Context, key string, oldValue, newValue T) error {
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		// Marshal the old and new values to JSON
+		oldValueBytes, err := json.Marshal(oldValue)
+		if err != nil {
+			return err
+		}
+
+		newValueBytes, err := json.Marshal(newValue)
+		if err != nil {
+			return err
+		}
+
+		fullKey := vc.buildPrefix(key)
+
+		return vc.client.CompareAndSwap(fullKey, string(oldValueBytes), string(newValueBytes))
+	})
+
+	return translateError(err)
+}
+
+func (vc *vcImpl[T]) Bump(ctx context.Context, key string, value int) error {
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		fullKey := vc.buildPrefix(key)
+		return vc.client.Bump(fullKey, value)
+	})
+
+	return translateError(err)
+}
+
+func (vc *vcImpl[T]) IterateByPrefix(ctx context.Context, prefix string, offset, limit int) ([]string, error) {
+	var result []string
+
+	err := client.WithRetriesVoid(ctx, vc.logger, func() error {
+		fullPrefix := vc.buildPrefix(prefix)
+		keys, err := vc.client.IterateByPrefix(fullPrefix, offset, limit)
+		if err != nil {
+			return err
+		}
+		result = keys
+		return nil
+	})
+
+	return result, translateError(err)
+}
